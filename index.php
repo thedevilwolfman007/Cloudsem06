abe5811befbabf2d944db20d99767494ee1a3110
Carlos Antonio Chumpitazi Cuya <u201311318@upc.edu.pe>
2017-05-05 04:23:06 +0000

    fff

--- index.php
+++ index.php
@@ -1,21 +1,40 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+ <html>
+ <head>
+  <title>PHP Starter Application</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link rel="stylesheet" href="style.css" />
+ </head>
+ <body>
+  <h1 style="text-align:center;">Alumnos</h1>
+  <table>
+   <?php
+  $servername = "us-cdbr-iron-east-03.cleardb.net";
+  $username = "b99b0a60c8e4d3";
+  $password = "06f20d07";
+  $dbname = "ad_7eb519e0bee50cb";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT * from alumnos";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {?>
+      <tr>
+      <td><?php echo $row['codigo']?></td>
+      <td><?php echo $row['nombre']?></td>
+      <td><?php echo $row['edad']?></td>
+      </tr>
+     <?php }
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+ ?> 
+ </table>
+ </body>
+ </html>
